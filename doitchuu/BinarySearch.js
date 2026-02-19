55f8efe31577786ecaf16f270ffdbeff8dc531c1
doitchuu <seulgichu14@gmail.com>
2026-02-20 08:20:49 +0900

    feat: BinarySearch 문제 풀이 작성

---
+++ doitchuu/BinarySearch.js
@@ -0,0 +1,27 @@
+function search(nums, target) {
+  return nums.indexOf(target);
+}
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+
+  while (start <= end) {
+    const middle = Math.floor((start + end) / 2);
+
+    if (nums[middle] === target) {
+      return middle;
+    } else if (nums[middle] > target) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+  }
+
+  return -1;
+};
