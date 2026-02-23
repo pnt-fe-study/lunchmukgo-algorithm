1a55c99fcb5647ab033110ecd83f4475b75ae5fb
sik9252 <sik9252@naver.com>
2026-02-23 21:34:58 +0900

    feat: BalancedBinaryTree 풀이 작성

---
+++ sik9252/BalancedBinaryTree.js
@@ -0,0 +1,19 @@
+var isBalanced = function (root) {
+  function getHeight(node) {
+    if (node === null) return 0;
+    return Math.max(getHeight(node.left), getHeight(node.right)) + 1;
+  }
+
+  function check(node) {
+    if (node === null) return true;
+
+    const leftH = getHeight(node.left);
+    const rightH = getHeight(node.right);
+
+    if (Math.abs(leftH - rightH) > 1) return false;
+
+    return check(node.left) && check(node.right);
+  }
+
+  return check(root);
+};
