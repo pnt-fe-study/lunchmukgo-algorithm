d1d0c40186960cf451d41260038d0480caab85d2
doitchuu <seulgichu14@gmail.com>
2026-02-28 17:14:14 +0900

    feat: BalancedBinaryTree 문제 풀이 작성

--- doitchuu/BalancedBinaryTree.js
+++ doitchuu/BalancedBinaryTree.js
@@ -11,5 +11,19 @@
  * @return {boolean}
  */
 var isBalanced = function(root) {
-    
+    function dfs(node) {
+        if (node === null) return 0;
+
+        const left = dfs(node.left);
+        if (left === -1) return -1;
+
+        const right = dfs(node.right);
+        if (right === -1) return -1;
+
+        if (Math.abs(left - right) > 1) return -1;
+
+        return Math.max(left, right) + 1;
+    }
+
+    return dfs(root) !== -1;
 };
