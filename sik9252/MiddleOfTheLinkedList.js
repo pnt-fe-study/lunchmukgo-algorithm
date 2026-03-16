84fb16468b718f0d382fac36622d82f3e0f7e9a0
sik9252 <sik9252@naver.com>
2026-03-16 19:39:52 +0900

    feat: Middle of the Linked List 풀이 작성

---
+++ sik9252/MiddleOfTheLinkedList.js
@@ -0,0 +1,15 @@
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+};
