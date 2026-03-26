3be8f8528eb6cb75e0708fc77cede47e2aba39b6
doitchuu <seulgichu14@gmail.com>
2026-03-27 08:17:56 +0900

    feat: Number of 1 Bits 풀이 작성

---
+++ doitchuu/NumberOf1Bits.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let current = n >>> 0;
+    let count = 0;
+
+    while (current !== 0) {
+        current &= current - 1;
+        count++;
+    }
+
+    return count;
+};
